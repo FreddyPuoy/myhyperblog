9f998b4484d563efe50c9aeb85418ee6f8f21d1c
Freddy Puoy <freddypuoy@gmail.com>
2023-08-04 22:00:43 -0300

    arranco mi proyecto real

---
+++ my-project/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='w-80 h-96 bg-slate-200 m-auto rounded-xl'>
+      <h1 className="text-6xl center font-bold underline text-amber-500">
+        Hello world!
+      </h1>
+      <p className='to-blue-600 bold text-xl'>Hola Tailwind</p>
+    </div>
+  )
+}
+
+export default App
