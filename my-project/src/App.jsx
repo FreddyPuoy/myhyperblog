b161262b7bd062bb51c4fed1fcaacd86de6d4cac
Freddy Puoy <freddypuoy@gmail.com>
2023-08-05 21:37:28 -0300

    estructura inicial de la cabecera

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,13 +2,20 @@ import './App.css'
 
 function App() {
   return (
-    <div className='w-3/4 h-96 bg-gray-950 m-auto rounded-xl py-8 px-8 gap-3 flex flex-col'>
-      <p className='uppercase text-gray-400 font-medium'>blogpost</p>
-      <h1 className="text-4xl center font-medium text-gray-100">
-        Este es un titulo atractivo del post
-      </h1>
-      <p className='text-gray-200 bold text-xl'>Este es un parrafo de inicio donde voy a escribir algo que seguramente te va a encantar</p>
+    <div className='w-full h-full p-10'>
+      <div className=''>
+        <p>cabecera</p>
+      </div>
+      <div className='w-3/4 h-96 bg-gray-950 m-auto rounded-xl py-8 px-8 gap-3 flex flex-col'>
+        <p className='uppercase text-gray-400 font-medium'>blogpost</p>
+        <h1 className="text-4xl center font-medium text-gray-100">
+          Este es un titulo atractivo del post
+        </h1>
+        <p className='text-gray-200 bold text-xl'>Este es un parrafo de inicio donde voy a escribir algo que seguramente te va a encantar</p>
+      </div>
     </div>
+
+
   )
 }
 
