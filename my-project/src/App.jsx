ddb76aa7c6cfd831ebb0a30f9676cd7b9b091e7b
Freddy Puoy <freddypuoy@gmail.com>
2023-08-05 21:27:56 -0300

    commit al master del blogpost en su version mas reciente

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,11 +2,12 @@ import './App.css'
 
 function App() {
   return (
-    <div className='w-80 h-96 bg-slate-200 m-auto rounded-xl'>
-      <h1 className="text-6xl center font-bold underline text-amber-500">
-        Hello world!
+    <div className='w-3/4 h-96 bg-gray-950 m-auto rounded-xl py-8 px-8 gap-3 flex flex-col'>
+      <p className='uppercase text-gray-400 font-medium'>blogpost</p>
+      <h1 className="text-4xl center font-medium text-gray-100">
+        Este es un titulo atractivo del post
       </h1>
-      <p className='to-blue-600 bold text-xl'>Hola Tailwind</p>
+      <p className='text-gray-200 bold text-xl'>Este es un parrafo de inicio donde voy a escribir algo que seguramente te va a encantar</p>
     </div>
   )
 }
